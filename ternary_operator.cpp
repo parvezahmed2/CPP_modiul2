83c0e03bb1a6cb1b3bca99965a6824da75151a39
kiparvez <ndp943296@gmail.com>
2023-05-22 18:48:52 +0600

    first commit

---
+++ ternary_operator.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int x=10;
+    // if(x%2==0){
+    //     cout<<"Even";  
+    // }
+    // else{
+    //     cout<<"odd";
+    // }
+
+
+    (x%2==0)?cout<<"Even" :cout<<"odd";
+    return 0;
+}
